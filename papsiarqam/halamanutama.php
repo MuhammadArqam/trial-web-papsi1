bd746d51184a84944541613b7db453bfa38dea59
MuhammadArqam <muhammadarqam1991@gmail.com>
2016-03-14 20:26:26 +0700

    Added files via upload

---
+++ papsiarqam/halamanutama.php
@@ -0,0 +1,54 @@
+<html>
+	<head>
+    	<title>This MyBlog </title>
+        <link href="css/bootstrap.css" rel="stylesheet">
+    </head>
+    <body>
+    	
+        <div class="container-fluid">
+				<h3 align="center">This My Blog</h3>
+                <h3 align="right"> <a href="">Login</a></h3>
+    <div class="row" style="margin-top:10px"  style=" width:400px">
+		<div class="col-md-12">
+			<ul class="nav nav-tabs">
+				<li class="active">
+					<a href="#">Halaman Utama</a>
+				</li>
+				<li>
+					<a href="#">Profile</a>
+				</li>
+				<li class="disabled">
+					<a href="#">SEM</a>
+				</li>
+                <li class="disabled">
+					<a href="#">Sistem Informasi</a>
+				</li>
+				
+					</ul>
+				</li>
+			</ul>
+		</div>
+	</div>
+</div>
+    <div class="container-fluid">
+	<div class="row"style="margin-top:50px">
+		<div class="col-md-2">
+			 <span class="label label-default">Selamat Datang di MyBlog</span>
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h3 class="panel-title">
+					<a href="">	GESCA </a>
+					</h3>
+				</div>
+				<div class="panel-body">
+					<a href="">SMARTPLS</a>
+				</div>
+				<div class="panel-footer">
+					<a href="">Model Delone and Mclean</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+    </body>
+</html>
