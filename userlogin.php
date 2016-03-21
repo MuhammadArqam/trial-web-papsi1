46caf359d6578b3ee11b124477f0aa1c95a3e51e
Muhammad Arqam <muhammadarqam1991@gmail.com>
2016-03-21 19:57:21 +0700

    Added files via upload

---
+++ userlogin.php
@@ -0,0 +1,46 @@
+<html>
+	<head>
+    	<title>This MyBlog </title>
+        <link href="css/bootstrap.css" rel="stylesheet">
+    </head>
+    <body>
+     <h1 align="center">Silahkan Login</h1>
+    
+<div class="container-fluid" style="margin-top:70px">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="row">
+				<div class="col-md-4">
+				</div>
+				<div class="col-md-4">
+					<form role="form">
+						<div class="form-group">
+							 
+							<label for="exampleInputEmail1">
+								user name
+							</label>
+							<input class="form-control" id="exampleInputUser" type="user" placeholder="username" />
+						</div>
+						<div class="form-group">
+							 
+							<label for="exampleInputPassword1">
+								Password
+							</label>
+							<input class="form-control" id="exampleInputPassword1" type="password" placeholder="password" />
+						</div> <br>
+						<div class="form-group">
+						<button type="submit" class="btn btn-default">
+							Submit
+						</button>
+					</form>
+                    <br><br>
+                    <label><a href="halamanutama.php">Kembali</a> </label>
+				</div>
+				<div class="col-md-4">
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
